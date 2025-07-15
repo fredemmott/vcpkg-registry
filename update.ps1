20a5fd9b6a7b3517f1a43ca6a898c4170d10c82a
Fred Emmott <git@fredemmott.com>
2025-07-15 15:56:23 -0500

    Support releases in update.ps1

--- update.ps1
+++ update.ps1
@@ -5,34 +5,56 @@
 #  - port has (and reads from) a  version.json with 'repo' and 'branch' properties
 #  - you want the latest commit, not a release
 #  - you're using `version-date` in your vcpkg file
-#
-# TODO: support tagged releases instead, via an option in the `version.json` file
-$Port = 'fredemmott-gui'
+[CmdletBinding()]
+param(
+  [Parameter(Mandatory=$True)]
+  [String]$Port,
+  [ValidateSet("Release")]
+  [string]$Source
+)
 $ConfigPath = "ports/${Port}/version.json"
-$Config = Get-Content $ConfigPath | ConvertFrom-Json
+$Config = Get-Content $ConfigPath | ConvertFrom-Json -AsHashtable
 $Repo = $Config.repo
-$CommitData = $(gh api /repos/${Repo}/commits?per_page=1 -q '.[0]') | ConvertFrom-Json
-$Commit = $CommitData.sha
-$CommitDate = $CommitData.commit.committer.date.ToString('yyyy-MM-dd')
-$TarballURL = "https://github.com/${Repo}/archive/${commit}.tar.gz"
 
 $VcpkgPath= "ports/${Port}/vcpkg.json"
-$Vcpkg = Get-Content $VcpkgPath | ConvertFrom-Json -Depth 10
-if (-not ($Vcpkg.'version-date'.StartsWith($CommitDate))) {
-  $Vcpkg.'version-date' = $CommitDate;
-} elseif ($Vcpkg['version-date'] -eq $CommitDate) {
-  $Vcpkg.'version-date' = "${CommitDate}.1"
+$Vcpkg = Get-Content $VcpkgPath | ConvertFrom-Json -Depth 10 -AsHashtable
+
+if ($Config.release -or $Source -eq "Release")
+{
+  $Commit = $( gh api /repos/${Repo}/releases -q '[.[] | select(.prerelease == false)].[0].tag_name' )
+  $Config.release = $Commit
+
+  $Config.Remove('commit')
+  foreach ($Key in $Vcpkg.keys.Clone()) {
+    if ($Key -match 'version-*') {
+      $Vcpkg.Remove($Key)
+    }
+  }
+  $Vcpkg.'version-semver' = $Commit -replace '^v',''
 } else {
-  $PreviousVersion = [int] $Vcpkg.'version-date'.split('.')[1]
-  $Version = $PreviousVersion + 1
-  $Vcpkg.'version-date' = "${CommitDate}.${Version}"
+  $CommitData = $(gh api /repos/${Repo}/commits?per_page=1 -q '.[0]') | ConvertFrom-Json
+  $Commit = $CommitData.sha
+  $Config.commit = $Commit
+  $CommitDate = $CommitData.commit.committer.date.ToString('yyyy-MM-dd')
+
+  if (-not ($Vcpkg.'version-date'.StartsWith($CommitDate))) {
+    $Vcpkg.'version-date' = $CommitDate;
+  } elseif ($Vcpkg['version-date'] -eq $CommitDate) {
+    $Vcpkg.'version-date' = "${CommitDate}.1"
+  } else {
+    $PreviousVersion = [int] $Vcpkg.'version-date'.split('.')[1]
+    $Version = $PreviousVersion + 1
+    $Vcpkg.'version-date' = "${CommitDate}.${Version}"
+  }
 }
+
 ConvertTo-Json $Vcpkg -Depth 10 | Set-Content $VcpkgPath -Encoding utf8NoBom
 vcpkg format-manifest $VcpkgPath
 
+$TarballURL = "https://github.com/${Repo}/archive/${Commit}.tar.gz"
+
 $WebRequest = Invoke-WebRequest -Uri $TarballURL -UseBasicParsing
 
-$Config.commit = $Commit
 $Config.sha512 = (Get-FileHash -InputStream $WebRequest.RawContentStream -Algorithm "SHA512").Hash.ToLower()
 (ConvertTo-Json $Config).Replace("`r`n","`n") | Set-Content $ConfigPath -Encoding utf8NoBOM
 
