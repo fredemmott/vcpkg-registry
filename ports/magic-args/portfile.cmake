361a03d50b9aca0eb06e14746e7616120d85fa1b
Fred Emmott <git@fredemmott.com>
2025-12-31 09:04:13 -0600

    Manual changes for magic_args develop branch
    fixes #1

--- ports/magic-args/portfile.cmake
+++ ports/magic-args/portfile.cmake
@@ -1,16 +1,32 @@
 file(READ "${CMAKE_CURRENT_LIST_DIR}/version.json" VERSION_JSON)
 string(JSON VERSION_REF GET "${VERSION_JSON}" "commit")
 string(JSON VERSION_SHA512 GET "${VERSION_JSON}" "sha512")
+string(JSON VERSION_BRANCH GET "${VERSION_JSON}" "branch")
 
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO fredemmott/magic_args
   REF ${VERSION_REF}
   SHA512 ${VERSION_SHA512}
-  HEAD_REF develop
+  HEAD_REF ${VERSION_BRANCH}
 )
 
 set(VCPKG_BUILD_TYPE release) # header-only
+
+vcpkg_check_features(
+  OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+  PREFIX FEATURE
+  FEATURES
+  charset-conversion ENABLE_ICONV
+  magic-enum ENABLE_MAGIC_ENUM
+)
+
+if (FEATURE_ENABLE_ICONV)
+  list(REMOVE_ITEM FEATURE_OPTIONS "-DENABLE_ICONV=ON")
+  # Use the win32 API for charset-conversion
+  list(APPEND FEATURE_OPTIONS "-DENABLE_ICONV=not-windows")
+endif ()
+
 
 vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
@@ -18,6 +34,7 @@ vcpkg_cmake_configure(
   OPTIONS
   -DBUILD_TESTING=OFF
   -DBUILD_EXAMPLES=OFF
+  ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
@@ -25,9 +42,19 @@ vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
+vcpkg_copy_tools(
+  TOOL_NAMES magic_args-enumerate-subcommands
+  AUTO_CLEAN
+)
+
 vcpkg_cmake_config_fixup(
   CONFIG_PATH
   "lib/cmake/magic_args"
-  PACKAGE_NAME "magic_args")
+  PACKAGE_NAME "magic_args"
+)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
+file(
+  REMOVE_RECURSE
+  "${CURRENT_PACKAGES_DIR}/lib"
+  "${CURRENT_PACKAGES_DIR}/debug/include"
+)
